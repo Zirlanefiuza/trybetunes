58a57439c1b296c83097450cb2d865c015f6bd4a
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-03-03 17:34:40 -0300

    requisito 5 concluido

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,48 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    inputArtist: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { inputArtist } = this.state;
+    const onDisable = 2;
+
     return (
       <div>
         <div data-testid="page-search" />
         <Header />
+
+        <form>
+
+          <label htmlFor="inputArtist">
+            Artist Search
+            <input
+              data-testid="search-artist-input"
+              id="inputArtist"
+              type="text"
+              name="inputArtist"
+              value={ inputArtist }
+              onChange={ this.handleChange }
+            />
+          </label>
+
+          <button
+            data-testid="search-artist-button"
+            disabled={ inputArtist.length < onDisable }
+          >
+            Pesquisar
+          </button>
+
+        </form>
+
       </div>
 
     );
