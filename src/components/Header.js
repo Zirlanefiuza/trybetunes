a6c097676509101d7beef3065038e5e1bb8dc7b8
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-03-03 16:15:36 -0300

    requisito 4 concluido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 import User from './User';
@@ -30,6 +31,22 @@ class Header extends Component {
           (loading) ? <Loading />
             : <User name={ userName } />
         }
+        <nav>
+          <ul>
+            <li>
+              <Link to="/search" data-testid="link-to-search">Search</Link>
+            </li>
+
+            <li>
+              <Link to="/favorites" data-testid="link-to-favorites">/Favorites</Link>
+            </li>
+
+            <li>
+              <Link to="/profile" data-testid="link-to-profile">/Profile</Link>
+            </li>
+          </ul>
+        </nav>
+
       </header>
 
     );
