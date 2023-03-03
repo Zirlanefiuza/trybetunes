88d4f07e73dcac1a575ab36b31efe7888d5629bd
Zirlanefiuza <zirlanebatistafiuza2@gmail.com>
2023-03-03 09:46:28 -0300

    atualização do requisito 1

--- src/App.js
+++ src/App.js
@@ -32,15 +32,15 @@ class App extends React.Component {
             />) }
           />
 
-          <Route path="/search" component={ Search } />
+          <Route exact path="/search" component={ Search } />
 
-          <Route path="/album/:id" component={ Album } />
+          <Route exact path="/album/:id" component={ Album } />
 
-          <Route path="/favorites" component={ Favorites } />
+          <Route exact path="/favorites" component={ Favorites } />
 
-          <Route path="/profile" component={ Profile } />
+          <Route exact path="/profile" component={ Profile } />
 
-          <Route path="/profile/edit" component={ ProfileEdit } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
 
           <Route component={ NotFound } />
 
